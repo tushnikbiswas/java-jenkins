b498db2352372a33a5699350a657b8f487b06335
Tushnik Biswas <robo.tushnik@gmail.com>
2024-06-22 00:45:23 +0530

    Java code added
    Java code added

---
+++ Java-Jenkins/src/Demo.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins");
+        System.out.println(new Date());
+        System.out.println("Hello Java");
+    }
+}
